34d160b93936c5bae54951303bb96ebc1c41b9d1
leschinskiy <topjumbo@mail.ru>
2026-04-02 14:59:21 +1000

    feat: добавить flake.nix для nix devShell с GTK4-зависимостями

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          rustc
+          cargo
+          clippy
+          rustfmt
+        ];
+
+        buildInputs = with pkgs; [
+          gtk4
+          libadwaita
+          glib
+          cairo
+          pango
+          gdk-pixbuf
+          graphene
+          dbus
+        ];
+      };
+    };
+}
